9b6176321d62ece21e41cb927bb1bd372c743e30
George Benjamin <george.benjamin@spglobal.com>
2024-06-20 18:22:22 +0300

    [fix:newo-tree] show hidden files by default

--- lua/adeotek/lazy/neo_tree.lua
+++ lua/adeotek/lazy/neo_tree.lua
@@ -1,13 +1,30 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim" 
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function()
-       --vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-       vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>', {})
-    end
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim" 
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+    	    hide_dotfiles = false,
+    	    hide_gitignored = false,
+	        hide_by_name = {
+	          -- '.git',
+  	        -- '.DS_Store',
+            -- 'thumbs.db',
+          },
+          never_show = {},
+        },
+      }
+    })
+    --vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', {})
+  end
 }
